a5dc672642e9b779883729b27ca772785fbddf7e
AngelahMbula <mbula.angelah@gmail.com>
2023-08-04 19:05:14 +0300

    finished the homepage

---
+++ app/components/Panel.jsx
@@ -0,0 +1,25 @@
+// components/Panel.js
+import React, { useState } from 'react';
+import { AiFillPlusSquare, AiFillMinusSquare } from 'react-icons/ai';
+
+const Panel = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="mb-4">
+      <div
+        className="flex justify-start space-x-3 items-center cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className="text-[#110000] hover:text-[#079DE2]">
+          {isOpen ? <AiFillMinusSquare size={30}/> : <AiFillPlusSquare size={30}/>}
+        </span>
+        <h2 className="text-lg text-[#110000] border-b border-[#110000]">{title}</h2>
+      </div>
+      {isOpen && <div className="mt-2 text-[#414565] md:ml-10 ml-5">{children}</div>}
+      {isOpen && <div className="mt-2"></div>}
+    </div>
+  );
+};
+
+export default Panel;
