b89576e0398da12f71dd85c999052799deef8381
marksikaundi <emmanuelmark117@gmail.com>
2023-08-07 20:47:32 +0200

    added about us page

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Businesses() {
+  return (
+    <div className="bg-white">
+      <h1 className="text-center text-[#6d6e71]">ABOUT US</h1>
+      
+    </div>
+  );
+}
