ceb046ee61e94001498ec5797710ce0d4f5595e0
marksikaundi <emmanuelmark117@gmail.com>
2023-08-07 19:28:18 +0200

    formated the codes

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,42 +1,38 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Image from 'next/image'
-
+import React from "react";
+import Navbar from "./Navbar";
+import Image from "next/image";
 
 function Header() {
   return (
-    <div className='h-fit border-1 max-w-full z-9999 bg-[#ffffff]'>
-        <div className='hidden sm:block md:flex justify-between w-4/5 px-20 m-auto py-4 items-center bg-[#ffffff]'>
-          <div className='left-0 top-0'>
-            <Image src="/WA.png" alt="logo" width="320" height="180" />
+    <div className="h-fit border-1 max-w-full z-9999 bg-[#ffffff]">
+      <div className="hidden sm:block md:flex justify-between w-4/5 px-20 m-auto py-4 items-center bg-[#ffffff]">
+        <div className="left-0 top-0">
+          <Image src="/WA.png" alt="logo" width="320" height="180" />
+        </div>
+        <div className="flex flex-wrap">
+          <div className="p-4">
+            <Image src="/ios.png" alt="logo" width="170" height="120" />
           </div>
-          <div className='flex flex-wrap'>
-            <div className='p-4'>
-              <Image src="/ios.png" alt="logo" width="170" height="120" /> 
-            </div>
-            <div className='p-4'>
-              <Image src="/googleplay.png" alt="logo" width="170" height="120" /> 
-            </div>
+          <div className="p-4">
+            <Image src="/googleplay.png" alt="logo" width="170" height="120" />
           </div>
-        <div className='block p-5 lg:flex flex-wrap flex-shrink space-x-2'>
-            <div>
-              <button className="bg-gray-500 text-white px-4 py-2 rounded">
-                Sign In
-              </button>
-            </div>
-            <div>
-              <button className="bg-black text-white px-4 py-2">
-                Sign Up
-              </button>
-            </div>
         </div>
+        <div className="block p-5 lg:flex flex-wrap flex-shrink space-x-2">
+          <div>
+            <button className="bg-gray-500 text-white px-4 py-2 rounded">
+              Sign In
+            </button>
+          </div>
+          <div>
+            <button className="bg-black text-white px-4 py-2">Sign Up</button>
+          </div>
         </div>
-        <div className='bottom-0 sm:flex flex-wrap bg-[#2b3c96] items-center'>
-            <Navbar/>
-        </div>
+      </div>
+      <div className="bottom-0 sm:flex flex-wrap bg-[#2b3c96] items-center">
+        <Navbar />
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Header
+export default Header;
--- app/components/ImageCarousel.jsx
+++ app/components/ImageCarousel.jsx
@@ -1,22 +1,26 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import { GrNext, GrPrevious } from 'react-icons/gr';
+import React, { useState, useEffect } from "react";
+import { GrNext, GrPrevious } from "react-icons/gr";
 
 const images = [
-  'https://corporate.workapp.world/wp-content/uploads/2016/04/banners-4.jpg',
-  'https://corporate.workapp.world/wp-content/uploads/2017/08/slider.jpg',
-  'https://corporate.workapp.world/wp-content/uploads/2017/08/banners-7.jpg',
+  "https://corporate.workapp.world/wp-content/uploads/2016/04/banners-4.jpg",
+  "https://corporate.workapp.world/wp-content/uploads/2017/08/slider.jpg",
+  "https://corporate.workapp.world/wp-content/uploads/2017/08/banners-7.jpg",
 ];
 
 const ImageCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
   };
 
   const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   useEffect(() => {
@@ -29,13 +33,20 @@ const ImageCarousel = () => {
 
   return (
     <div className="relative items-center justify-center">
-    <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} className="w-full"/>
+      <img
+        src={images[currentIndex]}
+        alt={`Slide ${currentIndex}`}
+        className="w-full"
+      />
       <div className="bg-gradient-to-r from-gray-500 absolute md:h-60 h-20 left-0 bottom-0 text-white font-bold md:text-3xl text-sm block">
         <h1 className="md:pt-6 pt-3 md:ml-20 ml-10">BUY, SELL,</h1>
         <p className="md:ml-20 ml-10">RENT or SWAP</p>
         <div className="md:ml-20 ml-10 md:pt-14 hidden md:flex">
-        <img src="https://corporate.workapp.world/wp-content/uploads/2017/08/workapp_btn.png" alt="..." />
-    </div>
+          <img
+            src="https://corporate.workapp.world/wp-content/uploads/2017/08/workapp_btn.png"
+            alt="..."
+          />
+        </div>
       </div>
       <div className="absolute bottom-0 left-0 right-0 flex justify-center space-x-2 mt-4">
         {images.map((_, index) => (
@@ -43,7 +54,7 @@ const ImageCarousel = () => {
             key={index}
             onClick={() => setCurrentIndex(index)}
             className={`h-2 w-2 rounded-full ${
-              index === currentIndex ? 'bg-blue-500' : 'bg-gray-300'
+              index === currentIndex ? "bg-blue-500" : "bg-gray-300"
             }`}
           ></button>
         ))}
@@ -52,7 +63,7 @@ const ImageCarousel = () => {
         onClick={prevSlide}
         className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-gray-500 p-2 md:h-10 md:w-10 border"
       >
-        <GrPrevious/>
+        <GrPrevious />
       </button>
       <button
         onClick={nextSlide}
