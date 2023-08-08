0843935393c6ead2e7996d42a243691af180cf79
marksikaundi <emmanuelmark117@gmail.com>
2023-08-08 05:50:13 +0200

    added divs

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 export default function Businesses() {
   return (
-    <div className="bg-white">
-      <h1 className="text-center text-[#6d6e71]">ABOUT US</h1>
-      
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div className="">
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+      </div>
     </div>
   );
 }
