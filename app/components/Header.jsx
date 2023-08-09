eadd13f3b593719fdcc6eb4f45633c92242e8159
marksikaundi <emmanuelmark117@gmail.com>
2023-08-09 08:18:54 +0200

    minor modification

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,10 +6,12 @@ function Header() {
   return (
     <div className="h-fit border-1 max-w-full z-9999 bg-[#ffffff]">
       <div className="w-full px-4 sm:px-6 lg:px-8 m-auto py-4 flex flex-col sm:flex-row items-center bg-[#ffffff]">
-        <div className="sm:order-1 mb-4 sm:mb-0 sm:w-1/3">
+        <div className="order-2 sm:order-1 mb-4 sm:mb-0 sm:w-1/3">
+          {/* Logo */}
           <Image src="/wa.png" alt="logo" width={320} height={180} />
         </div>
-        <div className="flex flex-wrap justify-center sm:justify-start sm:w-1/3">
+        <div className="order-1 sm:order-2 flex flex-wrap justify-center sm:justify-start sm:w-1/3">
+          {/* Download buttons */}
           <div className="p-2">
             <Image src="/ios.png" alt="logo" width={170} height={120} />
           </div>
@@ -17,7 +19,8 @@ function Header() {
             <Image src="/googleplay.png" alt="logo" width={170} height={120} />
           </div>
         </div>
-        <div className="mt-4 sm:mt-0 sm:w-1/3 flex justify-center sm:justify-end space-x-2">
+        <div className="order-3 sm:order-3 mt-4 sm:mt-0 sm:w-1/3 flex justify-center sm:justify-end space-x-2">
+          {/* Sign In and Sign Up buttons */}
           <button className="bg-gray-500 text-white px-4 py-2 rounded">
             Sign In
           </button>
